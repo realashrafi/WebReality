307ec2be56c2dfc058643fc266bb2e474165cdaa
ali <zboom0ne01@gmail.com>
2025-08-09 14:23:56 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import './App.css';
+import QrFixAr from "./projects/zboom/QrFixAr";
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<div>index</div>}/>
+                <Route path="/stage" element={<QrFixAr/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
